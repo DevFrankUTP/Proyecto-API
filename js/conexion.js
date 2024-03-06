7e3cf1e604ac0ff201cca199d08f6bb2e20a700a
DevFrankUTP <U19311061@utp.edu.pe>
2024-03-06 17:51:38 -0500

    actualicación

--- js/conexion.js
+++ js/conexion.js
@@ -1,26 +1,26 @@
 
-let mysql = require("mysql");
+const mysql = require("mysql");
 
-let conexion = mysql.createConnection({
+const conexion = mysql.createConnection({
     host: "localhost",
     database: "componentes",
     user: "root",
     password:""
 });
 
-
-
-conexion.connect(function(error){
-
+/* conexion.connect(function(error){
 
     if(error){
-        /*Si existe error */
         throw error;
     }else{
-        /*Si no hay error */
         console.log("conexion exitosa");
     }
-});
+})*/
+
+conexion.connect( (error) => {
+    if(error) throw error
+    console.log("conexion exitosa");
+}) 
 
 
 
@@ -31,14 +31,19 @@ conexion.connect(function(error){
 
 const productos="SELECT * FROM productos";
 
-conexion.query(productos,function(error,lista){
+conexion.query(productos,function(error,datos){
 
     if(error){
         /*Si existe error */
         throw error;
     }else{
         /*Si no hay error imprimir datos de la BD */
-        console.log(lista);
+        console.log("Estos son los datos de la tabla: ");
+        console.log(datos);
+
+        console.log("La cantidad de datos es: ");
+        console.log(datos.length);
+
     }
 });
 
